937cbec3338c9305345e698940e548ee170af6d1
futureds <futureds@2uin.com>
2022-06-04 16:02:02 +0900

    feat(middleware): 미들웨어 기능 추가
    미들웨어 기능 추가

--- server/src/router/user/index.js
+++ server/src/router/user/index.js
@@ -3,6 +3,10 @@ import * as userApi from "./user.ctrl";
 
 const router = new KoaRouter();
 
-router.get("/", userApi.get);
+router.get("/", userApi.search);
+router.get("/:id", userApi.getParams, userApi.get);
+router.post("/", userApi.create);
+router.put("/:id", userApi.getParams, userApi.getBody, userApi.update);
+router.delete("/:id", userApi.remove);
 
 export default router;
--- server/src/router/user/user.ctrl.js
+++ server/src/router/user/user.ctrl.js
@@ -1,12 +1,116 @@
-// eslint-disable-next-line no-unused-vars
-const get = async (ctx, next) => {
-  ctx.status = 200;
-  ctx.body = {
-    message: "ok",
+import UserDb from "@/database/user";
+
+export const getParams = async (ctx, next) => {
+  const { id } = ctx.params;
+
+  ctx.state.params = {
+    id,
   };
+
+  await next();
 };
 
-export {
-  // eslint-disable-next-line import/prefer-default-export
-  get,
+export const getBody = async (ctx, next) => {
+  const {
+    id, password, name, role,
+  } = ctx.request.body;
+
+  ctx.state.body = {
+    id,
+    password,
+    name,
+    role,
+  };
+
+  await next();
+};
+
+// eslint-disable-next-line no-unused-vars
+export const search = async (ctx, next) => {
+  try {
+    const items = await UserDb.find().exec();
+
+    ctx.status = 200;
+    ctx.body = {
+      items,
+    };
+  } catch (e) {
+    ctx.status = 500;
+  }
+};
+
+export const get = async (ctx, next) => {
+  try {
+    const { id } = ctx.state.params;
+
+    const response = await UserDb.findOne({ _id: id }).exec();
+
+    ctx.status = 200;
+    ctx.body = response;
+  } catch (e) {
+    ctx.status = 500;
+    console.log(e);
+  }
+};
+
+export const create = async (ctx, next) => {
+  try {
+    const { id, password, name } = ctx.request.body;
+
+    const newUser = new UserDb({
+      id, password, name,
+    });
+
+    const response = await newUser.save();
+
+    ctx.status = 200;
+    ctx.body = response;
+  } catch (e) {
+    ctx.status = 500;
+    console.log(e);
+  }
+};
+
+export const update = async (ctx, next) => {
+  try {
+    const { id } = ctx.state.params;
+    const { password, name, role } = ctx.state.body;
+
+    await UserDb.updateOne(
+      // 검색 조건
+      {
+        _id: id,
+      },
+      // 업데이트 구문
+      {
+        $set:
+        {
+          password,
+          name,
+          role,
+        },
+      },
+    ).exec();
+
+    const response = await UserDb.findOne({ _id: id }).exec();
+
+    ctx.status = 200;
+    ctx.body = response;
+  } catch (e) {
+    ctx.status = 500;
+    console.log(e);
+  }
+};
+
+export const remove = async (ctx, next) => {
+  try {
+    const { id } = ctx.params;
+
+    await UserDb.remove({ _id: id });
+
+    ctx.status = 200;
+  } catch (e) {
+    ctx.status = 500;
+    console.log(e);
+  }
 };
