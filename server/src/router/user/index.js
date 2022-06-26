dac015151a5893046811c42c4e7068d1587c7f73
futureds <futureds@2uin.com>
2022-06-26 19:33:00 +0900

    feat(mongoose-query-parser):  설치
    .

--- server/src/router/user/index.js
+++ server/src/router/user/index.js
@@ -1,9 +1,10 @@
 import KoaRouter from "koa-router";
+import getMongooseQueryParser from "../middleware/getMongooseQueryParser";
 import * as userApi from "./user.ctrl";
 
 const router = new KoaRouter();
 
-router.get("/", userApi.getQueryString, userApi.search);
+router.get("/", getMongooseQueryParser, userApi.search);
 router.get("/:id", userApi.getParams, userApi.get);
 router.post("/", userApi.create);
 router.put("/:id", userApi.getParams, userApi.getBody, userApi.update);
--- server/src/router/user/user.ctrl.js
+++ server/src/router/user/user.ctrl.js
@@ -64,11 +64,21 @@ export const getBody = async (ctx, next) => {
 
 export const search = async (ctx) => {
   try {
-    console.log(ctx.state.query);
-    const { offset, limit, sort } = ctx.state.query;
+    const { mongoQuery } = ctx.state;
 
-    const items = await UserDb.find().skip(offset).limit(limit).sort(sort)
-      .exec();
+    // const items = await UserDb.find().skip(offset).limit(limit).sort(sort)
+    //   .exec();
+
+    console.log(mongoQuery);
+
+    let newFind = UserDb.find(mongoQuery.filter).limit(mongoQuery.limit);
+
+    if (mongoQuery.select) newFind = newFind.select(mongoQuery.select);
+    if (mongoQuery.skip) newFind = newFind.skip(mongoQuery.skip);
+    if (mongoQuery.populate) newFind = newFind.populate(mongoQuery.populate);
+    if (mongoQuery.sort) newFind = newFind.sort(mongoQuery.sort);
+
+    const items = await newFind.exec();
 
     ctx.status = 200;
     ctx.body = {
