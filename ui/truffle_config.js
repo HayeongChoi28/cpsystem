12046720f4782825493fc1f70a36796066aa7104
trisuasu <trisha227@naver.com>
2022-11-18 15:46:57 +0900

    config 완성

--- ui/truffle_config.js
+++ ui/truffle_config.js
@@ -1,1 +1,23 @@
-//가나슈 주소.
+require("babel-register");
+require("babel-polyfill");
+
+module.exports = {
+  networks: {
+    developments: {
+      host: "127.0.0.1",
+      port: "7545",
+      network_id: "*",
+    },
+  },
+  contracts_directory: "./src/contracts/",
+  contracts_build_directory: "./src/truffle_abis/",
+  compilers: {
+    solc: {
+      vesrion: "^0.5.0",
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
