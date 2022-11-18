4e7e02148f3be2c6bc9400742b0f655e744217d9
trisuasu <trisha227@naver.com>
2022-11-18 16:17:54 +0900

    run 하다 말았음

--- ui/truffle_config.js
+++ ui/truffle_config.js
@@ -13,7 +13,7 @@ module.exports = {
   contracts_build_directory: "./src/truffle_abis/",
   compilers: {
     solc: {
-      vesrion: "^0.5.0",
+      vesrion: "^0.8.4",
       optimizer: {
         enabled: true,
         runs: 200,
